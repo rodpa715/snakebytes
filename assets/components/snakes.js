bdae8c7d1fb4353fec57daceb2eec538eefa5ae7
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-17 20:04:24 +0200

    add method for creating elements and initate Instance

--- assets/components/snakes.js
+++ assets/components/snakes.js
@@ -1,98 +1,75 @@
 const snakeDatabase = [{
-    name: "Cobra",
-    kingdom: "Animalia",
-    phylum: "Reptilia",
-    order: "Squamata",
+    name: "King Cobra",
+    danger: "Very Dangerous",
+    img: "https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Reptiles/H-P/king-cobra-hood.adapt.945.1.jpg",
+    kill: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+},
+{
+    name: "Calabar python",
+    danger: "Dangerous",
+    img: "https://steemitimages.com/DQmSkjLrpo8RWpjrtfrfgsDPpty9tz7dUD1V1EdZQpbr4Wm/%D1%81%D0%B4%D1%84%D0%B8%D1%84%D0%BA2.jpg",
     suborder: "Serpentes",
-    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+    kill: "\nThis species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.\nThe toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis.\nIf the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+},
+{
+    name: "Boa Constrictor",
+    danger: "Danerous",
+    img: "https://lafeber.com/vet/wp-content/uploads/boa-constrictor-by-gjofilli.jpg",
+    kill: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+},
+{
+    name: "Anaconda",
+    danger: "Animalia",
+    img: "https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Reptiles/H-P/king-cobra-hood.adapt.945.1.jpg",
+    kill: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+},
+{
+    name: "Naja",
+    danger: "Animalia",
+    img: "https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Reptiles/H-P/king-cobra-hood.adapt.945.1.jpg",
+    kill: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
 },
 {
     name: "Cobra2",
-    kingdom: "Animalia",
-    phylum: "Reptilia",
-    order: "Squamata",
-    suborder: "Serpentes",
-    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+    danger: "Animalia",
+    img: "https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Reptiles/H-P/king-cobra-hood.adapt.945.1.jpg",
+    kill: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
 },
 {
     name: "Cobra2",
-    kingdom: "Animalia",
-    phylum: "Reptilia",
-    order: "Squamata",
-    suborder: "Serpentes",
-    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+    danger: "Animalia",
+    img: "https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Reptiles/H-P/king-cobra-hood.adapt.945.1.jpg",
+    kill: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
 },
 {
     name: "Cobra2",
-    kingdom: "Animalia",
-    phylum: "Reptilia",
-    order: "Squamata",
-    suborder: "Serpentes",
-    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+    danger: "Animalia",
+    img: "https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Reptiles/H-P/king-cobra-hood.adapt.945.1.jpg",
+    kill: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
 },
 {
     name: "Cobra2",
-    kingdom: "Animalia",
-    phylum: "Reptilia",
-    order: "Squamata",
-    suborder: "Serpentes",
-    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+    danger: "Animalia",
+    img: "https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Reptiles/H-P/king-cobra-hood.adapt.945.1.jpg",
+    kill: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
 },
 {
     name: "Cobra2",
-    kingdom: "Animalia",
-    phylum: "Reptilia",
-    order: "Squamata",
-    suborder: "Serpentes",
-    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+    danger: "Animalia",
+    img: "https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Reptiles/H-P/king-cobra-hood.adapt.945.1.jpg",
+    kill: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
 },
 {
     name: "Cobra2",
-    kingdom: "Animalia",
-    phylum: "Reptilia",
-    order: "Squamata",
-    suborder: "Serpentes",
-    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
-},
-{
-    name: "Cobra2",
-    kingdom: "Animalia",
-    phylum: "Reptilia",
-    order: "Squamata",
-    suborder: "Serpentes",
-    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
-},
-{
-    name: "Cobra2",
-    kingdom: "Animalia",
-    phylum: "Reptilia",
-    order: "Squamata",
-    suborder: "Serpentes",
-    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
-},
-{
-    name: "Cobra2",
-    kingdom: "Animalia",
-    phylum: "Reptilia",
-    order: "Squamata",
-    suborder: "Serpentes",
-    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
-},
-{
-    name: "Cobra2",
-    kingdom: "Animalia",
-    phylum: "Reptilia",
-    order: "Squamata",
-    suborder: "Serpentes",
-    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+    danger: "Animalia",
+    img: "https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Reptiles/H-P/king-cobra-hood.adapt.945.1.jpg",
+    kill: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
 },
 {
     name: "Cobra3",
-    kingdom: "Animalia",
-    phylum: "Reptilia",
-    order: "Squamata",
-    suborder: "Serpentes",
-    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+    danger: "Animalia",
+    img: "https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Reptiles/H-P/king-cobra-hood.adapt.945.1.jpg",
+    kill: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
 }]
 
 class SnakeList{
@@ -108,7 +85,7 @@ class SnakeList{
         const snakeListHeading = document.createElement("h2");
         this.snakeListComponent = document.createElement("div");
         this.snakeListComponent.classList.add("snake-list-component");
-        snakeListHeading.textContent = "Snakes";
+        snakeListHeading.textContent = "SNAKES";
         snakeList.appendChild(snakeListHeading);
         snakeList.appendChild(this.snakeListComponent);
         this.createAnchors();
@@ -118,17 +95,46 @@ class SnakeList{
         const snakeInfoHeading = document.createElement("h2");
         this.snakeInfoComponent = document.createElement("div");
         this.snakeInfoComponent.classList.add("snake-info-component");
-        snakeInfoHeading.textContent = "Facts"
+        snakeInfoHeading.textContent = "AMAZING FACTS"
         snakeInfo.appendChild(snakeInfoHeading)
         snakeInfo.appendChild(this.snakeInfoComponent)
+        this.createInnerElements()
     }
+    createInnerElements(){
+        const imageWrapper =  document.createElement("div")
+        imageWrapper.classList.add("image-wrapper")
+        this.snakeInfoComponent.appendChild(imageWrapper)
+
+        const image = document.createElement("img")
+        image.classList.add("snake", "img")
+        imageWrapper.appendChild(image)
+
+        const textWrapper = document.createElement("div")
+        textWrapper .classList.add("text-wrapper")
+        this.snakeInfoComponent.appendChild(textWrapper )
+
+        const name = document.createElement("h3")
+        name.classList.add("snake", "name")
+        textWrapper.appendChild(name)
+
+        const danger = document.createElement("h3")
+        danger.classList.add("snake", "danger")
+        textWrapper.appendChild(danger)
+
+        const killingMethod = document.createElement("p")
+        killingMethod.classList.add("snake" ,"kill")
+        textWrapper.appendChild(killingMethod)
+
+        this.elements = this.snakeInfoComponent.querySelectorAll(".snake")
+
+    }   
     createAnchors(){
-        console.log(this.snakesData)
         const snakeAnchors = []
-        this.snakesData.forEach( (idx) => {
+        this.snakesData.forEach( (snakeObject) => {
             let snake = document.createElement("a")
-            snake.textContent = idx.name
+            snake.textContent = snakeObject.name
             this.snakeListComponent.appendChild(snake)
+            snake.addEventListener("click", () => new SnakeInfo(snakeObject, this.elements))
             snakeAnchors.push(snake)
         });
     }
