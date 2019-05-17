09a9d0c71ae942ea7017f0277a0a9e9372bca216
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-17 17:45:11 +0200

    add data to object & anchor component

--- assets/components/snakes.js
+++ assets/components/snakes.js
@@ -21,6 +21,78 @@ const snakeDatabase = [{
     order: "Squamata",
     suborder: "Serpentes",
     venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+},
+{
+    name: "Cobra2",
+    kingdom: "Animalia",
+    phylum: "Reptilia",
+    order: "Squamata",
+    suborder: "Serpentes",
+    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+},
+{
+    name: "Cobra2",
+    kingdom: "Animalia",
+    phylum: "Reptilia",
+    order: "Squamata",
+    suborder: "Serpentes",
+    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+},
+{
+    name: "Cobra2",
+    kingdom: "Animalia",
+    phylum: "Reptilia",
+    order: "Squamata",
+    suborder: "Serpentes",
+    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+},
+{
+    name: "Cobra2",
+    kingdom: "Animalia",
+    phylum: "Reptilia",
+    order: "Squamata",
+    suborder: "Serpentes",
+    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+},
+{
+    name: "Cobra2",
+    kingdom: "Animalia",
+    phylum: "Reptilia",
+    order: "Squamata",
+    suborder: "Serpentes",
+    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+},
+{
+    name: "Cobra2",
+    kingdom: "Animalia",
+    phylum: "Reptilia",
+    order: "Squamata",
+    suborder: "Serpentes",
+    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+},
+{
+    name: "Cobra2",
+    kingdom: "Animalia",
+    phylum: "Reptilia",
+    order: "Squamata",
+    suborder: "Serpentes",
+    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+},
+{
+    name: "Cobra2",
+    kingdom: "Animalia",
+    phylum: "Reptilia",
+    order: "Squamata",
+    suborder: "Serpentes",
+    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
+},
+{
+    name: "Cobra3",
+    kingdom: "Animalia",
+    phylum: "Reptilia",
+    order: "Squamata",
+    suborder: "Serpentes",
+    venom: "This species is capable of delivering a fatal bite and the victim may receive a large quantity of venom with a dose of 200 to 500 mg up to 7 ml.<br> The toxins affect the victim's central nervous system, resulting in severe pain, blurred vision, vertigo, drowsiness, and eventually paralysis. If the envenomation is serious, it progresses to cardiovascular collapse, and the victim falls into a coma. Death soon follows due to respiratory failure."
 }]
 
 class SnakeList{
@@ -33,28 +105,31 @@ class SnakeList{
     }
     constructSnakeList(){
         const snakeList = this.parentElement.querySelector(".snake-list");
-        const snakeListComponent = document.createElement("div");
         const snakeListHeading = document.createElement("h2");
-        snakeListComponent.classList.add("snake-list-component");
+        this.snakeListComponent = document.createElement("div");
+        this.snakeListComponent.classList.add("snake-list-component");
         snakeListHeading.textContent = "Snakes";
         snakeList.appendChild(snakeListHeading);
-        snakeList.appendChild(snakeListComponent);
+        snakeList.appendChild(this.snakeListComponent);
         this.createAnchors();
     }
     constructSnakeInfo(){
         const snakeInfo = this.parentElement.querySelector(".snake-info");
-        const snakeInfoComponent = document.createElement("div");
         const snakeInfoHeading = document.createElement("h2");
-        snakeInfoComponent.classList.add("snake-info-component");
+        this.snakeInfoComponent = document.createElement("div");
+        this.snakeInfoComponent.classList.add("snake-info-component");
         snakeInfoHeading.textContent = "Facts"
         snakeInfo.appendChild(snakeInfoHeading)
-        snakeInfo.appendChild(snakeInfoComponent)
+        snakeInfo.appendChild(this.snakeInfoComponent)
     }
     createAnchors(){
-        this.snakeAnchors = this.snakesData.forEach(snake => {
-            document.createElement("a")
-            snake.textContent = this.snakesData.name;
-            
+        console.log(this.snakesData)
+        const snakeAnchors = []
+        this.snakesData.forEach( (idx) => {
+            let snake = document.createElement("a")
+            snake.textContent = idx.name
+            this.snakeListComponent.appendChild(snake)
+            snakeAnchors.push(snake)
         });
     }
 }
